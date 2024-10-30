f3c91b9156768cddd8382136f66e94eda1cf4dcc
Ryan Small <Ryan-Small@harbinger.tech>
2024-10-30 19:40:28 -0400

    added base_href property for testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    { provide: APP_BASE_HREF, useValue: "/workflow" },
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
